8ea9f19cc87cb82e912da825464f52ed6c077758
Roman Musin <roman9m@yandex.ru>
2022-02-04 16:04:20 +0300

    fix port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const rateLimiter = require('./middlewares/rateLimit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { dataBse, NODE_ENV } = process.env;
-const { PORT = 3000 } = process.env;
+const { PORT = 4000 } = process.env;
 const app = express();
 
 mongoose.connect(
