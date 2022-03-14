a952a9dc47ec7b029fbef96280e269900f940316
Roman Musin <roman9m@yandex.ru>
2022-03-14 19:01:44 +0300

    new try

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const rateLimiter = require('./middlewares/rateLimit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { dataBse, NODE_ENV } = process.env;
-const { PORT = 3030 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(helmet());
 
@@ -22,28 +22,28 @@ mongoose.connect(
   NODE_ENV === 'production' ? dataBse : 'mongodb://localhost:27017/moviesdb',
 );
 
-// app.use(
-//   '*',
-//   cors({
-//     origin: [
-//       'https://moviex.nomoredomains.work',
-//       'http://moviex.nomoredomains.work',
-//       'http://localhost:3000',
-//       'https://localhost:3000',
-//     ],
-//     methods: ['OPTIONS', 'GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-//     allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Cookie'],
-//     // exposedHeaders: ['Set-Cookie'],
-//     credentials: true,
-//   }),
-// );
+app.use(
+  '*',
+  cors({
+    origin: [
+      'https://moviex.nomoredomains.work',
+      'http://moviex.nomoredomains.work',
+      'http://localhost:3000',
+      'https://localhost:3000',
+    ],
+    methods: ['OPTIONS', 'GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Cookie'],
+    // exposedHeaders: ['Set-Cookie'],
+    credentials: true,
+  }),
+);
 
-app.use(cors({
-  origin: 'https://moviex.nomoredomains.work',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'https://moviex.nomoredomains.work',
+//   credentials: true,
+// }));
 
 app.use(cookieParser());
 app.use(express.json());
