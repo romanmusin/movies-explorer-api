ea2d7695a9c4d3a05f89f1bbb45aca2ec7f6ab8e
Roman Musin <roman9m@yandex.ru>
2022-03-16 00:25:52 +0300

    cors update

--- app.js
+++ app.js
@@ -28,10 +28,37 @@ mongoose.connect(NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017
   useUnifiedTopology: true,
 });
 
-app.use(cors({
-  origin: NODE_ENV === 'production' ? 'https://moviex.nomoredomains.work' : 'http://localhost:3000',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: NODE_ENV === 'production' ? 'https://moviex.nomoredomains.work' : 'http://localhost:3000',
+//   credentials: true,
+// }));
+
+const allowedCors = [
+  'http://moviex.nomoredomains.work',
+  'https://moviex.nomoredomains.work',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.status(200).send();
+  }
+
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
